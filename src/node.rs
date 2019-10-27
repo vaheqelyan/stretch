a8632e0a202d85de631a62ae85af60721897724d
Vahe <vahe.araqelyan.2013@hotmail.com>
2019-10-27 17:51:45 +0400

    Update node.rs

--- src/node.rs
+++ src/node.rs
@@ -220,10 +220,10 @@ impl Stretch {
         Ok(self.forest.nodes[id].is_dirty)
     }
     
-    pub fn is_text_node(&self, node: Node) -> Result<bool, Error> {
+    /*pub fn is_text_node(&self, node: Node) -> Result<bool, Error> {
         let id = self.find_node(node)?;
         Ok(self.forest.nodes[id].is_text)
-    }
+    }*/
     
     /*pub fn get_text_value(&self, node: Node) -> Result<String, Error> {
         let id = self.find_node(node)?;
