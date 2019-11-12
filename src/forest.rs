a4a2e65ac98e533e7e1669a5c341a3002325c620
Vahe <vahe.araqelyan.2013@hotmail.com>
2019-11-12 15:04:13 +0400

    Update forest.rs

--- src/forest.rs
+++ src/forest.rs
@@ -189,9 +189,13 @@ impl Forest {
         fn set_offset(nodes: &mut Vec<NodeData>, node_id: NodeId, offset: f32) {
             let node = &mut nodes[node_id];
             
+            let bound_height = node.bottom - node.y;
+            
+            let real_max_size = node.cache_farest_element - bound_height;
+            
             node.offset += offset;
             
-            node.offset = clamp(node.offset, 0.0, node.cache_farest_element);
+            node.offset = clamp(node.offset, 0.0, real_max_size);
             
             println!("{:?}", node.bottom - node.y);
             
